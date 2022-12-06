7102b39c6760cb5f01a0aa4570e9299ed498e5df
soulflamecompany <soulflamemusic@gmail.com>
2022-12-06 04:14:41 +0300

    add css module and props

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,17 @@
-function Card() {
+import styles from "./Card.module.scss";
+
+function Card(props) {
   return (
-    <div className="card">
-      <div className="favorite">
+    <div className={styles.card}>
+      <div className={styles.favorite}>
         <img src="/img/heart-unliked.svg" alt="Unliked" />
       </div>
-      <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers" />
-      <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+      <img width={133} height={112} src={props.imageUrl} alt="Sneakers" />
+      <h5>{props.title}</h5>
       <div className="d-flex justify-between align-center">
         <div className="d-flex flex-column ">
           <span>Цена:</span>
-          <b>12 999 руб.</b>
+          <b>{props.price} руб.</b>
         </div>
         <button className="button">
           <img src="/img/btn-plus.svg" alt="Plus" />
